2040a791d8435e1c8ffe171686eba7e8e215f0dc
Aakov <Aakov@10.100.197.72>
2016-04-08 02:33:05 +0800

    Updated QueryManager

--- src/db_services/QueryManager.java
+++ src/db_services/QueryManager.java
@@ -1,6 +1,10 @@
 package db_services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import db_manager.DBConnection;
 
 public class QueryManager {
 	public QueryManager(){}
@@ -8,6 +12,16 @@ public class QueryManager {
 	// Process the query here -> READ and WRITE
 	public ResultSet processQuery(String query){
 		
+		Connection conn = DBConnection.getConnection();
+		
+		try{
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			
+			return pstmt.executeQuery();	// Returns the result set done by the query
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
